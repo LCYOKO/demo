3069a5f4650687ab908d32e1619b3cdbcb48cc20
liuchiyun <m15879038981@163.com>
2023-12-25 21:21:06 +0800

    add

--- dao/testdb.go
+++ dao/testdb.go
@@ -1,22 +1,31 @@
-package testgorm
+package dao
 
 import (
+	"database/sql"
 	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"time"
 )
 
 var db *gorm.DB
 
 type User struct {
 	gorm.Model
-	Id   int64
-	Name string
-	Age  int
+	Name         string
+	Email        *string
+	Age          uint8
+	Birthday     *time.Time
+	MemberNumber sql.NullString
+	ActivatedAt  sql.NullTime
+}
+
+func (u *User) TableName() string {
+	return "user"
 }
 
 // https://gorm.io/docs/index.html
-func init() {
+func init()  {
 	var err error
 	dsn := "root:lcyoko@tcp(114.55.147.178:33060)/test?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
@@ -29,9 +38,9 @@ func TestCreate() {
 	u := &User{
 		Name: "lisi",
 	}
-	db.Create(u)
-
+	create := db.Create(u)
+	fmt.Println("u", create.RowsAffected)
 	var user User
 	db.First(&user)
-	fmt.Println(user)
+	fmt.Println("user", user)
 }
