1c7c987258047d320374f6f2aabcb4c4d95807a5
liuchiyun <m15879038981@163.com>
2024-09-29 18:05:37 +0800

    add

--- study/basic/basic/time_test.go
+++ study/basic/basic/time_test.go
@@ -37,7 +37,6 @@ func TestTime2(t *testing.T) {
 	//shanghai, err := time.LoadLocation("Asia/Shanghai") // UTC+08:00
 	// 加载东京所在的时区
 	//tokyo, err := time.LoadLocation("Asia/Tokyo") // UTC+09:00
-
 	// 创建时间对象需要指定位置。常用的位置是 time.Local（当地时间） 和 time.UTC（UTC时间）。
 	// timeInLocal := time.Date(2009, 1, 1, 20, 0, 0, 0, time.Local) // 系统本地时间
 	timeInUTC := time.Date(2009, 1, 1, 12, 0, 0, 0, time.UTC)
@@ -47,7 +46,6 @@ func TestTime2(t *testing.T) {
 	// 北京时间（东八区）比UTC早8小时，所以上面两个时间看似差了8小时，但表示的是同一个时间
 	timesAreEqual := timeInUTC.Equal(sameTimeInBeijing)
 	fmt.Println(timesAreEqual)
-
 	// 纽约（西五区）比UTC晚5小时，所以上面两个时间看似差了5小时，但表示的是同一个时间
 	timesAreEqual = timeInUTC.Equal(sameTimeInNewYork)
 	fmt.Println(timesAreEqual)
@@ -87,4 +85,12 @@ func TestTimer(t *testing.T) {
 	timer.Reset(3 * time.Second)
 	<-timer.C
 	fmt.Println("timer fired")
+}
+
+func TestTicker(t *testing.T) {
+	ticker := time.NewTicker(time.Second)
+	for {
+		<-ticker.C
+		fmt.Println("hello")
+	}
 }
